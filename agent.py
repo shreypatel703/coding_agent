7e2049ceb3eb51bf3566a23193e68206f74009b6
shreypatel703 <shreypatel703@gmail.com>
2025-02-13 11:01:05 -0500

    initial server

---
+++ agent.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+import os
+
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Hello World"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json  # Get JSON payload
+    print("Received Webhook Data:", data)
+    return jsonify({"message": "Webhook received!"}), 200
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
