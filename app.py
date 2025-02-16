3070c33e51f6427118c325d3b31515ed4c17a138
shreypatel703 <shreypatel703@gmail.com>
2025-02-15 22:46:58 -0500

    added new label webhook functionality to manually trigger commenting and test creation. Also split off llm and pygithub code into seperate files for readability

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 from flask import Flask, request, jsonify
 from PR_agent import handlePullRequestOpened
-
+from test_agent import handlePullRequestForTestAgent
 
 app = Flask(__name__)
 
@@ -17,12 +17,21 @@ def webhook():
     with open("data.json", "w") as f:
         json.dump(data,f, indent=4)
     try:
+        event = request.headers.get("X-GitHub-Event")
+        action = data.get("action", None)
         # The event type is in the header. We're primarily looking for "pull_request" events.
-        eventType = data.get("pull_request", None)
-
         # For a "pull_request" event, if the action is "opened", we handle it with our function above.
-        if eventType and data.get("action", None) == "opened":
-            handlePullRequestOpened(data)
+        if event == "pull_request":
+            if action == "opened":
+                handlePullRequestOpened(data)
+            elif action == "labeled":
+                labelName = data.get("label").get("name")
+                if labelName == "agent-review-pr":
+                    handlePullRequestOpened(data)
+                elif labelName == "agent-generate-tests":
+                    handlePullRequestForTestAgent(data)
+
+
         return jsonify({"message": "OK!"}), 200
     except Exception as error:
         print("Error processing webhook:", error)
--- github_auth.py
+++ github_auth.py
@@ -1,6 +1,7 @@
 import os
 import jwt
 import time
+import base64
 import requests
 from dotenv import load_dotenv
 
@@ -23,11 +24,12 @@ def generate_jwt():
 
     payload = {
         "iat": int(time.time()),  # Issued at time
-        "exp": int(time.time()) + 600,  # Expires in 10 minutes
+        "exp": int(time.time()) + 60,  # Expires in 10 minutes
         "iss": GITHUB_APP_ID,  # GitHub App ID
     }
 
     return jwt.encode(payload, private_key, algorithm="RS256")
+
 
 
 def generate_installation_token():
@@ -45,3 +47,29 @@ def generate_installation_token():
     installation_token = token_response.json()["token"]
     
     return installation_token
+
+def postPlaceholderComment(repository, pullNumber: int, text:str):
+    
+    # Post a comment on the PR
+    try:
+        comment = repository.get_issue(pullNumber).create_comment(text)
+        return comment
+    except Exception as ex:
+        print("Error Creating Placeholder Comment for pull:", pullNumber)
+        raise Exception("Error Creating Placeholder Comment for pull:", pullNumber, "\nText:", text)
+
+def updateComment(commentObj, body):
+    commentObj.edit(body)
+    
+def getFileContent(file, repository, ref):
+    content = None
+    if file.status != "removed":
+        try:
+            file_content = repository.get_contents(file.filename, ref)
+            # Decode base64 content
+            if hasattr(file_content, "content") and isinstance(file_content.content, str):
+                content = base64.b64decode(file_content.content).decode("utf-8")
+
+        except Exception as e:  
+            print(f"Error retrieving content for {file.filename}:", e)
+    return content
